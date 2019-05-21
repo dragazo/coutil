a1b67fce40e6e2d56e57ecb5bdefaa133ecb3051
dragazo <37459229+dragazo@users.noreply.github.com>
2019-05-21 11:55:45 -0500

    task exception handling + redid test file
    added exception handling to basic_task.
    without a definition for unhandled_exception() in the promise type, apparently compilers are free to not use a try-catch around the coroutine body.
    this means the final await will not be triggered and the task will be destroyed.
    this was leading to undefined memory accesses in the new exception test cases.
    
    this is easily fixed by defining unhandled_exception() and forwarding a std::exception_ptr through the wait() method.
    thus, this propagates coroutine exception upon .wait() or co_await (which uses .wait()).
    additionally, (so long as it's defined) immediately after calling unhandled_exception() the coroutine enters the final suspend and is therefore done().
    so most of the code doesn't need to change.
    
    removed the .get() method from basic_task<T> (where T is not void).
    this is replaced with .wait() to be more similar to basic_task<void> (which only has .wait()).
    also, i didn't like .get() because its name doesn't necessarily imply that it blocks for completion.
    i think it's clearer this way.
    
    changed non-void basic_task<T> to use std::variant for holding the T object (and the exception pointer).
    this is less error prone because i don't have to concern myself with the lifetime of whatever type it contains, and it (potentially) saves space.
    by making std::exception_ptr the first type in the std::variant we have the same behavior as the buffer, where T doesn't have to be default-constructible.
    
    found a typo where basic_task<T>::resume() could resume past the final suspend (was using co.done() instead of !co.done()).
    
    reintroduced the commented-out static_assert in non-void basic_task<T> that ensures T is not a reference type.
    if references need to be returned, use std::reference_wrapper<T>.
    i might (but probably won't) specialize cases for returning references - if i do it'll likely just be sfinae magic to be more concise.
    
    changed task<T> and lazy_task<T> alias templates to put T through std::remove_cv_t.
    this is done so that we're not storing const/volatile objects inside the promise.
    this also has the benefit of making task<void> the same as task<const void> etc.
    the cv qualifiers of a return type aren't really important anyway.
    
    redid the test file to be shortened by macros and also not print anything to the screen unless there's an error (failed test case).
    test cases now use co_routine lambdas to be even more concise (they're so pretty...).

--- coutil.h
+++ coutil.h
@@ -5,6 +5,7 @@
 #include <exception>
 #include <stdexcept>
 #include <type_traits>
+#include <variant>
 #include <experimental/coroutine>
 
 namespace coutil
@@ -24,29 +25,26 @@ namespace coutil
 
 	// ----------- //
 
-	// a basic_task represents a coroutine that co_returns a (single) value of type T.
-	// InitialSuspend is the type to return for initial_suspend().
+	// basic_task represents a coroutine that co_returns a (single) value of type T.
+	// if an exception is thrown, it is caught and rethrown upon awaiting the result.
+	// T              - the return type of the coroutine.
+	// InitialSuspend - the type to return for initial_suspend() (empty brace initialized).
 	template<typename T, typename InitialSuspend>
 	class basic_task
 	{
 	public: // -- promise -- //
 
-		//static_assert(!std::is_reference_v<T>, "T in basic_task<T> cannot be of reference type");
+		static_assert(!std::is_reference_v<T>, "T in basic_task<T> cannot be of reference type");
 
 		struct promise_type;
 		typedef std::experimental::coroutine_handle<promise_type> handle;
 
 		struct promise_type
 		{
-			alignas(T) char buffer[sizeof(T)];   // buffer to hold the return value
-			bool            constructed = false; // marks if the buffer object is constructed
+			// this holds the state information about this coroutine (nothing, ret, or exception)
+			std::variant<std::exception_ptr, T> stat;
 
-			~promise_type()
-			{
-				if (constructed) reinterpret_cast<T*>(buffer)->~T();
-			}
-
-			basic_task get_return_object() { return basic_task{ handle::from_promise(*this) }; }
+			auto get_return_object() { return basic_task{ handle::from_promise(*this) }; }
 
 			auto initial_suspend() const noexcept { return InitialSuspend{}; }
 			auto final_suspend() const noexcept { return std::experimental::suspend_always{}; }
@@ -54,8 +52,12 @@ namespace coutil
 			template<typename U>
 			void return_value(U &&u)
 			{
-				new (buffer) T(std::forward<U>(u));
-				constructed = true;
+				stat.emplace<1>(std::forward<U>(u));
+			}
+
+			void unhandled_exception()
+			{
+				stat.emplace<0>(std::current_exception());
 			}
 		};
 
@@ -94,7 +96,7 @@ namespace coutil
 		// returns true if the basic_task is empty
 		bool operator!() const { return empty(); }
 
-		// returns true if the coroutine has completed execution.
+		// returns true if the coroutine has completed execution (successfully or due to exception).
 		// if the basic_task is currently empty, throws bad_coroutine_access.
 		bool done() const
 		{
@@ -109,40 +111,36 @@ namespace coutil
 		void resume()
 		{
 			if (empty()) throw bad_coroutine_access("Accessing empty couroutine manager");
-			if (co.done()) co.resume();
+			if (!co.done()) co.resume();
 		}
 
 	public: // -- value access -- //
 
-		// blocks until completion of the coroutine - returns *this.
+		// blocks until completion of the coroutine and gets the returned value.
 		// if the basic_task is currently empty, throws bad_coroutine_access.
-		void wait() const&
+		// if the coroutine ended due to exception, rethrows the exception.
+		const T &wait() const&
 		{
 			if (empty()) throw bad_coroutine_access("Accessing empty couroutine manager");
 			while (!co.done()) co.resume();
-		}
-		void wait() && = delete;
 
-		// blocks until completion of the coroutine and gets the returned value.
-		// if the basic_task is currently empty, throws bad_coroutine_access.
-		const T &get() const&
-		{
-			if (empty()) throw bad_coroutine_access("Accessing empty couroutine manager");
-			wait();
-			return reinterpret_cast<T&>(co.promise().buffer);
+			if (auto ret = std::get_if<1>(&co.promise().stat)) return *ret;
+			else std::rethrow_exception(std::get<0>(co.promise().stat));
 		}
-		T get() &&
+		T wait() &&
 		{
 			if (empty()) throw bad_coroutine_access("Accessing empty couroutine manager");
-			wait();
-			return reinterpret_cast<T&&>(co.promise().buffer);
+			while (!co.done()) co.resume();
+
+			if (auto ret = std::get_if<1>(&co.promise().stat)) return std::move(*ret);
+			else std::rethrow_exception(std::get<0>(co.promise().stat));
 		}
 
 	public: // -- await interface -- //
 
 		bool await_ready() { return done(); }
 		void await_suspend(std::experimental::coroutine_handle<>) {}
-		T    await_resume() { return get(); }
+		T    await_resume() { return wait(); }
 	};
 	template<typename InitialSuspend>
 	class basic_task<void, InitialSuspend>
@@ -154,12 +152,17 @@ namespace coutil
 
 		struct promise_type
 		{
+			// the exception thrown during coroutine execution (if any)
+			std::exception_ptr ex;
+
 			basic_task get_return_object() { return basic_task{ handle::from_promise(*this) }; }
 
 			auto initial_suspend() const noexcept { return InitialSuspend{}; }
 			auto final_suspend() const noexcept { return std::experimental::suspend_always{}; }
 
 			void return_void() {}
+
+			void unhandled_exception() { ex = std::current_exception(); }
 		};
 
 	private: // -- data -- //
@@ -197,7 +200,7 @@ namespace coutil
 		// returns true if the basic_task is empty
 		bool operator!() const { return empty(); }
 
-		// returns true if the coroutine has completed execution.
+		// returns true if the coroutine has completed execution (successfully or due to exception).
 		// if the basic_task is currently empty, throws bad_coroutine_access.
 		bool done() const
 		{
@@ -212,34 +215,36 @@ namespace coutil
 		void resume()
 		{
 			if (empty()) throw bad_coroutine_access("Accessing empty couroutine manager");
-			if (co.done()) co.resume();
+			if (!co.done()) co.resume();
 		}
 
 	public: // -- value access -- //
 
 		// blocks until completion of the coroutine - returns *this.
 		// if the basic_task is currently empty, throws bad_coroutine_access.
-		void wait() const&
+		// if the coroutine ended due to exception, rethrows the exception.
+		void wait() const
 		{
 			if (empty()) throw bad_coroutine_access("Accessing empty couroutine manager");
 			while (!co.done()) co.resume();
+
+			if (co.promise().ex) std::rethrow_exception(co.promise().ex);
 		}
-		void wait() && = delete;
 
 	public: // -- await interface -- //
 
 		bool await_ready() { return done(); }
 		void await_suspend(std::experimental::coroutine_handle<>) {}
-		void await_resume() { return wait(); }
+		void await_resume() { wait(); }
 	};
 
 	// a task is a basic_task which starts immediately and suspends
 	template<typename T = void>
-	using task = basic_task<T, std::experimental::suspend_never>;
+	using task = basic_task<std::remove_cv_t<T>, std::experimental::suspend_never>;
 
 	// a lazy_task is a basic_task which suspends immediately upon creation
 	template<typename T = void>
-	using lazy_task = basic_task<T, std::experimental::suspend_always>;
+	using lazy_task = basic_task<std::remove_cv_t<T>, std::experimental::suspend_always>;
 }
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,34 +1,68 @@
 #include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <type_traits>
+#include <cassert>
 #include <experimental/coroutine>
 
 #include "coutil.h"
 
 using namespace coutil;
 
-task<int> async_add(int a, int b) { co_return a + b; }
-lazy_task<int> foo()
+// asserts that expr throws an exception of type ex
+#define assert_throws(expr, ex) try { (expr); std::cerr << "DID NOT THROW: " #expr "\n"; std::terminate(); } \
+                                catch (const ex&) {} \
+                                catch (const std::exception &e) { std::cerr << "THREW WRONG TYPE " #ex ": " #expr "\nCAUSE: " << e.what() << '\n'; std::terminate(); } \
+                                catch (...) { std::cerr << "THREW WRONG TYPE " #ex ": " #expr "\n"; std::terminate(); }
+// asserts that expr throws an exception (of any type)
+#define assert_throws_any(expr) try { (expr); std::cerr << "DID NOT THROW: " #expr "\n"; std::terminate(); } \
+                                catch (...) {}
+// asserts that expr does not throw an exception
+#define assert_nothrow(expr) try { (expr); } \
+                             catch (const std::exception &e) { std::cerr << "THREW: " #expr "\nCAUSE: " << e.what() << '\n'; std::terminate(); } \
+                             catch (...) { std::cerr << "THREW: " #expr "\n"; std::terminate(); }
+
+int main() try
 {
-	std::cerr << "computing\n";
-	int v = co_await async_add(4, 5);
-	return v;
-}
-lazy_task<> lazy_print(const char *str)
-{
-	std::cerr << "lazy print: " << str << '\n';
-	co_return;
-}
+	static_assert(std::is_same_v<task<void>, task<const void>>);
+	static_assert(std::is_same_v<task<void>, task<volatile void>>);
+	static_assert(std::is_same_v<task<void>, task<const volatile void>>);
 
-int main()
-{
-	auto msg = lazy_print("hello world");
+	static_assert(std::is_same_v<lazy_task<void>, lazy_task<const void>>);
+	static_assert(std::is_same_v<lazy_task<void>, lazy_task<volatile void>>);
+	static_assert(std::is_same_v<lazy_task<void>, lazy_task<const volatile void>>);
 
-	auto f = foo();
-	std::cerr << "fetching value\n";
-	int v = f.get();
-	std::cerr << "got: " << v << '\n';
+	{
+		int p = 4;
+		task<> co = [](int &p) -> task<> { p = 44; co_return; }(p);
+		assert(co.done() && p == 44);
+		co.wait();
+		assert(co.done() && p == 44);
+	}
+	{
+		int p = 6;
+		lazy_task<> co = [](int &p) -> lazy_task<> { p = 77; co_return; }(p);
+		assert(!co.done() && p == 6);
+		co.wait();
+		assert(co.done() && p == 77);
+	}
 
-	msg.wait();
+	assert([](int a, int b) -> task<int> { co_return a + b; }(6, 7).wait() == 13);
+	
+	assert_throws([]() -> task<int> { throw 6; co_return 77; }().wait(), int);
+	assert_throws([]() -> lazy_task<int> { throw 6; co_return 77; }().wait(), int);
+	assert_throws([]() -> task<> { throw 6; co_return; }().wait(), int);
+	assert_throws([]() -> lazy_task<> { throw 6; co_return; }().wait(), int);
 
-	std::cin.get();
+	std::cout << "all tests completed\n";
+
 	return 0;
+}
+catch (const std::exception &ex)
+{
+	std::cerr << "UNHANDLED EXCEPTION: " << ex.what() << '\n';
+}
+catch (...)
+{
+	std::cerr << "UNHANDLED EXCEPTION\n";
 }
