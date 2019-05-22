51726cd78edf0477f1e74a6778cc3dca33412502
dragazo <37459229+dragazo@users.noreply.github.com>
2019-05-22 11:46:52 -0500

    added basic_generator
    added the basic_generator class.
    it has an experimental feature that i'm probably going to remove later on (just want it safe in a commit somewhere):
    the iterator (nested) class upon increment returns a pseudo_iterator object, which performs the work of doing the actual increment.
    the increment is performed either asynchronously via supporting co_await semantics or synchronously when destroyed, whichever comes first.
    the benefit of this is that it could theoretically work both for normal ++it and co_await ++it (as a for co_await statement would translate to).
    however the support for the async form is not entirely completed - i don't really feel like completin it because i know i'm going to scrap it anyway.
    the issue is that the result of ++it could be stored in an auto variable declaration, which means the destructor isn't called when i expect it, so the increment is subverted.
    additionally, it's not kosher to return something other than iterator& from an iterator increment overload.
    
    i'm planning on making basic_generator take additional template parameters to say whether it should be sync or async.
    this will be wrapped in generator<T> and async_generator<T> (or something similar) whenever it's added.
    
    added the is_task and is_task_v type traits for determining if some type is a (potentially cv-qualified) basic_task type.
    added wait_all() and wait_any() - they only participate in overload resolution if all their (param pack) args at task types.
    
    changed the custom assertion macros to also write the line number of the assertion failure.

--- coutil.h
+++ coutil.h
@@ -6,6 +6,7 @@
 #include <stdexcept>
 #include <type_traits>
 #include <variant>
+#include <iterator>
 #include <experimental/coroutine>
 
 namespace coutil
@@ -41,7 +42,7 @@ namespace coutil
 
 		struct promise_type
 		{
-			// this holds the state information about this coroutine (nothing, ret, or exception)
+			// this holds the state information about this coroutine (ret, or exception)
 			std::variant<std::exception_ptr, T> stat;
 
 			auto get_return_object() { return basic_task{ handle::from_promise(*this) }; }
@@ -67,11 +68,11 @@ namespace coutil
 
 	private: // -- private util -- //
 
-		basic_task(handle h) : co(h) {}
+		explicit basic_task(handle h) : co(std::move(h)) {}
 
 	public: // -- ctor / dtor / asgn -- //
 
-		// constructs an empty basic_task (does not refer to the return value of any coroutine)
+		// constructs an empty basic_task (does not refer to any existing coroutine)
 		basic_task() = default;
 
 		// destroys the currently-held coroutine handle (if any)
@@ -171,7 +172,7 @@ namespace coutil
 
 	private: // -- private util -- //
 
-		basic_task(handle h) : co(h) {}
+		explicit basic_task(handle h) : co(h) {}
 
 	public: // -- ctor / dtor / asgn -- //
 
@@ -212,7 +213,7 @@ namespace coutil
 
 		// if the coroutine is not finished, resumes it, otherwise does nothing.
 		// if the basic_task is currently empty, throws bad_coroutine_access.
-		void resume()
+		void resume() const
 		{
 			if (empty()) throw bad_coroutine_access("Accessing empty couroutine manager");
 			if (!co.done()) co.resume();
@@ -245,6 +246,272 @@ namespace coutil
 	// a lazy_task is a basic_task which suspends immediately upon creation
 	template<typename T = void>
 	using lazy_task = basic_task<std::remove_cv_t<T>, std::experimental::suspend_always>;
+
+	namespace detail
+	{
+		// gets if type T is any kind of basic_task
+		template<typename T>
+		struct _is_task : std::false_type {};
+		template<typename T, typename InitialSuspend>
+		struct _is_task<basic_task<T, InitialSuspend>> : std::true_type {};
+	}
+
+	// gets if type T is any kind of (potentially cv-qualified) basic_task
+	template<typename T>
+	using is_task = detail::_is_task<std::remove_cv_t<T>>;
+	template<typename T>
+	inline constexpr bool is_task_v = is_task<T>::value;
+	
+	template<typename ...T, std::enable_if_t<std::conjunction_v<is_task<T>...>, int> = 0>
+	void wait_all(const T &...tasks)
+	{
+		while ((... | (tasks.resume(), !tasks.done())));
+	}
+	template<typename ...T, std::enable_if_t<std::conjunction_v<is_task<T>...>, int> = 0>
+	void wait_any(const T &...tasks)
+	{
+		while (!(... | (tasks.resume(), tasks.done())));
+	}
+
+	// ---------------- //
+
+	// -- generators -- //
+
+	// ---------------- //
+
+	// basic_generator represents a coroutine that co_yields zero or more values T and optionally co_returns (nothing) to end execution.
+	// has the ability to iterate over the generated sequence of values as a (single-pass) input iterator.
+	// execution of the coroutine body does not begin until a yield value is requested.
+	template<typename T>
+	class basic_generator
+	{
+	public: // -- promise -- //
+
+		static_assert(!std::is_reference_v<T>, "T in basic_generator<T> cannot be of reference type");
+
+		struct promise_type;
+		typedef std::experimental::coroutine_handle<promise_type> handle;
+
+		struct promise_type
+		{
+			std::variant<std::exception_ptr, T> stat; // holds the state information about this coroutine (ret or exception)
+			bool yield_flag = false;                  // flag used to mark when a yield value is obtained
+
+			auto get_return_object() { return basic_generator{ handle::from_promise(*this) }; }
+
+			auto initial_suspend() const noexcept { return std::experimental::suspend_always{}; }
+			auto final_suspend() const noexcept { return std::experimental::suspend_always{}; }
+
+			template<typename U>
+			auto yield_value(U &&u)
+			{
+				// if we already have a T object, move assign it - otherwise move construct it
+				if (T *p = std::get_if<1>(&stat)) *p = std::forward<U>(u);
+				else stat.emplace<1>(std::forward<U>(u));
+
+				// mark that we now have a yield value
+				yield_flag = true;
+
+				// suspend so that we can use it
+				return std::experimental::suspend_always{};
+			}
+
+			void unhandled_exception()
+			{
+				stat.emplace<0>(std::current_exception());
+			}
+		};
+
+	private: // -- data -- //
+
+		handle co; // the raw coroutine handle
+
+	private: // -- private util -- //
+
+		basic_generator(handle h) : co(std::move(h)) {}
+
+	public: // -- ctor / dtor / asgn -- //
+
+		// constructs an empty basic_generator (does not refer to any existing coroutine)
+		basic_generator() = default;
+
+		// destroys the currently-held coroutine handle (if any)
+		~basic_generator() { if (co) co.destroy(); }
+
+		basic_generator(const basic_generator&) = delete;
+		basic_generator &operator=(const basic_generator&) = delete;
+
+		// steals the handle of other - other is left in the empty state
+		basic_generator(basic_generator &&other) : co(std::exchange(other.co, nullptr)) {}
+		// discards the current coroutine handle and steals other's - other is left in the empty state.
+		// on self assignment, does nothing.
+		basic_generator &operator=(basic_generator &&other) { co = std::exchange(other.co, nullptr); return *this; }
+
+	public: // -- state information -- //
+
+		// returns true if the basic_generator is currently in the empty state.
+		bool empty() const { return !co; }
+
+		// returns true if the basic_generator is non-empty
+		explicit operator bool() const { return !empty(); }
+		// returns true if the basic_generator is empty
+		bool operator!() const { return empty(); }
+
+	public: // -- iterator -- //
+
+		class iterator
+		{
+		public: // -- pseudo-iterator -- //
+
+			class pseudo_iterator
+			{
+			private: // -- data -- //
+
+				iterator *it;
+
+			private: // -- private util -- //
+
+				friend class iterator;
+
+				// constructs a pseudo_iterator to advance the given iterator.
+				// the provided iterator must have a valid (non-null) coroutine handle.
+				explicit pseudo_iterator(iterator &i) : it(&i)
+				{
+					// clear the yield flag and resume execution of the coroutine
+					it->co.promise().yield_flag = false;
+					it->co.resume();
+				}
+
+				// returns true if the increment process has completed
+				bool done()
+				{
+					// if the coroutine has finished (no yield value) or we got a yield value, we're done with the increment process
+					return it->co.done() || it->co.promise().yield_flag;
+				}
+				// blocks until completion of the increment process and updates the iterator source.
+				// after everything is completed, sets this pseudo-iterator to the empty state.
+				void wait()
+				{
+					// wait for completion of the increment process
+					while (!done()) it->co.resume();
+					
+					// if the coroutine has finished execution (no yield value) destroy the coroutine and null it.
+					// this effectively sets the iterator it was sourced from to the end iterator state.
+					if (it->co.done()) { it->co.destroy(); it->co = nullptr; }
+
+					// we're done now - no longer need the coroutine handle.
+					// null the raw pointer we have so that the destructor won't try to wait() again
+					it = nullptr;
+				}
+
+			public: // -- ctor / dtor / asgn -- //
+
+				// blocks and advances the stored iterator to the next yield value
+				~pseudo_iterator()
+				{
+					if (it) wait();
+				}
+
+				pseudo_iterator(const pseudo_iterator&) = delete;
+				pseudo_iterator &operator=(const pseudo_iterator&) = delete;
+
+				// passes on the responsibility for advancing the stored iterator to a new pseudo_iterator.
+				pseudo_iterator(pseudo_iterator &&other) : it(std::exchange(other.it, nullptr)) {}
+				pseudo_iterator &operator=(pseudo_iterator&&) = delete;
+
+			public: // -- await interface -- //
+
+				bool await_ready() { return done(); }
+				void await_suspend(std::experimental::coroutine_handle<>) {}
+				void await_result() { wait(); }
+			};
+
+		public: // -- iterator traits -- //
+
+			typedef std::input_iterator_tag iterator_category;
+			typedef T                       value_type;
+			typedef std::ptrdiff_t          difference_type;
+			typedef T                      *pointer_type;
+			typedef T                      &reference_type;
+
+		private: // -- data -- //
+
+			handle co; // the raw coroutine handle (from the generator)
+
+		private: // -- private util -- //
+
+			friend class basic_generator;
+
+			explicit iterator(handle h) : co(std::move(h))
+			{
+				// if we have a good coroutine, advance once to get the first value
+				if (co) ++*this;
+			}
+
+		public: // -- ctor / dtor / asgn -- //
+
+			iterator() = default;
+
+			~iterator() { if (co) co.destroy(); }
+
+			iterator(const iterator&) = delete;
+			iterator &operator=(const iterator&) = delete;
+
+			// steals the iteration handle from other - other is "empty" after this.
+			iterator(iterator &&other) : co(std::exchange(other.co, nullptr)) {}
+			// discards the current iterator handle and steals other's - other is "empty" after this.
+			// self assignment is no-op.
+			iterator &operator=(iterator &&other) { co = std::exchange(other.co, nullptr); return *this; }
+
+		public: // -- iterator interface -- //
+
+			// gets the currently-stored (most-recently-yielded) value.
+			// if an exception caused the coroutine to end prematurely, rethrows the exception.
+			const T &operator*() const&
+			{
+				// if we have an exception, rethrow it
+				if (auto ex = std::get_if<0>(&co.promise().stat); ex && *ex) std::rethrow_exception(*ex);
+
+				return std::get<1>(co.promise().stat);
+			}
+			T operator*() &&
+			{
+				// if we have an exception, rethrow it
+				if (auto ex = std::get_if<0>(&co.promise().stat); ex && *ex) std::rethrow_exception(*ex);
+
+				return std::move(std::get<1>(co.promise().stat));
+			}
+
+			// advances to the next yield value.
+			// this can be performed asyncronously with co_await.
+			// otherwise the destruction of the pseudo_iterator performs the operation synchronously.
+			// exceptions from this action are propagated through the dereferencing operator.
+			pseudo_iterator operator++() &
+			{
+				// make sure we have a valid (non-null) coroutine handle
+				if (!co) throw std::invalid_argument("Attempt to increment past end iterator");
+				return pseudo_iterator{*this};
+			}
+			pseudo_iterator operator++() && = delete;
+
+			friend bool operator==(const iterator &a, const iterator &b)
+			{
+				// iterator is a uniquely-owning type, so the only way they can be equal is if they're both invalid (end) iterators
+				return !a.co && !b.co;
+			}
+			friend bool operator!=(const iterator &a, const iterator &b) { return !(a == b); }
+		};
+
+		// gets the begin iterator of this generator.
+		// this operation steals the coroutine handle from this object - i.e. the generator is empty after this operation.
+		// if the generator is currently empty, returns the end iterator.
+		iterator begin() { return iterator{ std::exchange(co, nullptr) }; }
+		// returns the end iterator for this generator - this generator is not modified by this action.
+		iterator end() const { return iterator{}; }
+	};
+
+	template<typename T>
+	using generator = basic_generator<T>;
 }
 
 #endif
--- test.cpp
+++ test.cpp
@@ -9,18 +9,23 @@
 
 using namespace coutil;
 
+// given a macro name, converts its value into a string.
+// e.g. STR(__LINE__) would expand to "15" on line 15.
+#define STR(x) _STR(x)
+#define _STR(x) #x
+
 // asserts that expr throws an exception of type ex
-#define assert_throws(expr, ex) try { (expr); std::cerr << "DID NOT THROW: " #expr "\n"; std::terminate(); } \
+#define assert_throws(expr, ex) try { (expr); std::cerr << "LINE " STR(__LINE__) " DID NOT THROW: " #expr "\n"; std::terminate(); } \
                                 catch (const ex&) {} \
-                                catch (const std::exception &e) { std::cerr << "THREW WRONG TYPE " #ex ": " #expr "\nCAUSE: " << e.what() << '\n'; std::terminate(); } \
-                                catch (...) { std::cerr << "THREW WRONG TYPE " #ex ": " #expr "\n"; std::terminate(); }
+                                catch (const std::exception &e) { std::cerr << "LINE " STR(__LINE__) " THREW WRONG TYPE " #ex ": " #expr "\nCAUSE: " << e.what() << '\n'; std::terminate(); } \
+                                catch (...) { std::cerr << "LINE " STR(__LINE__) " THREW WRONG TYPE " #ex ": " #expr "\n"; std::terminate(); }
 // asserts that expr throws an exception (of any type)
-#define assert_throws_any(expr) try { (expr); std::cerr << "DID NOT THROW: " #expr "\n"; std::terminate(); } \
+#define assert_throws_any(expr) try { (expr); std::cerr << "LINE " STR(__LINE__) " DID NOT THROW: " #expr "\n"; std::terminate(); } \
                                 catch (...) {}
 // asserts that expr does not throw an exception
 #define assert_nothrow(expr) try { (expr); } \
-                             catch (const std::exception &e) { std::cerr << "THREW: " #expr "\nCAUSE: " << e.what() << '\n'; std::terminate(); } \
-                             catch (...) { std::cerr << "THREW: " #expr "\n"; std::terminate(); }
+                             catch (const std::exception &e) { std::cerr << "LINE " STR(__LINE__) " THREW: " #expr "\nCAUSE: " << e.what() << '\n'; std::terminate(); } \
+                             catch (...) { std::cerr << "LINE " STR(__LINE__) " THREW: " #expr "\n"; std::terminate(); }
 
 int main() try
 {
@@ -31,6 +36,41 @@ int main() try
 	static_assert(std::is_same_v<lazy_task<void>, lazy_task<const void>>);
 	static_assert(std::is_same_v<lazy_task<void>, lazy_task<volatile void>>);
 	static_assert(std::is_same_v<lazy_task<void>, lazy_task<const volatile void>>);
+
+	{
+		int val = 0;
+		auto a = [&]() -> task<>
+		{
+			assert(val == 0);
+			val = 14;
+			co_await std::experimental::suspend_always{};
+			assert(val == 65);
+			val = -56;
+			co_await std::experimental::suspend_always{};
+			assert(val == -128);
+			val = 365;
+			co_await std::experimental::suspend_always{};
+			assert(val == 12);
+			val = 19;
+		}();
+		auto b = [&]() -> task<>
+		{
+			assert(val == 14);
+			val = 65;
+			co_await std::experimental::suspend_always{};
+			assert(val == -56);
+			val = -128;
+			co_await std::experimental::suspend_always{};
+			assert(val == 365);
+			val = 12;
+			co_await std::experimental::suspend_always{};
+			assert(val == 19);
+			val = 1777;
+		}();
+
+		wait_all(a, b);
+		assert(val == 1777);
+	}
 
 	{
 		int p = 4;
@@ -53,6 +93,70 @@ int main() try
 	assert_throws([]() -> lazy_task<int> { throw 6; co_return 77; }().wait(), int);
 	assert_throws([]() -> task<> { throw 6; co_return; }().wait(), int);
 	assert_throws([]() -> lazy_task<> { throw 6; co_return; }().wait(), int);
+
+	{
+		generator<int> gen123 = []() -> generator<int> { co_yield 1; co_yield 2; co_yield 3; }();
+		auto beg = gen123.begin();
+		auto end = gen123.end();
+
+		assert(beg != end);
+		assert(*beg == 1);
+		assert_nothrow(++beg);
+
+		assert(beg != end);
+		assert(*beg == 2);
+		assert_nothrow(++beg);
+
+		assert(beg != end);
+		assert(*beg == 3);
+		assert_nothrow(++beg);
+		assert(beg == end);
+		assert_throws(++beg, std::invalid_argument);
+		assert_throws(++end, std::invalid_argument);
+	}
+	{
+		generator<int> gen_1_stop = []() -> generator<int> { co_yield 7; co_return; co_yield 8; }();
+		auto end = gen_1_stop.end();
+		auto beg = gen_1_stop.begin();
+		auto beg2 = gen_1_stop.begin();
+		
+		assert(beg2 == end);
+
+		assert(beg != end);
+		assert(*beg == 7);
+		assert_nothrow(++beg);
+
+		assert(beg == end);
+		assert(beg == beg2);
+		assert_throws(++beg, std::invalid_argument);
+		assert_throws(++end, std::invalid_argument);
+		assert_throws(++beg2, std::invalid_argument);
+	}
+	{
+		generator<int> empty = []() -> generator<int> { co_return; }();
+		assert(empty.begin() == empty.end());
+	}
+	{
+		generator<int> my_iota = []() -> generator<int> { for (int i = 0;;) co_yield i++; }();
+		auto beg = my_iota.begin();
+		auto end = my_iota.end();
+		
+		for (int i = 0; i < 20; ++i)
+		{
+			assert(*beg == i);
+			assert_nothrow(++beg);
+		}
+	}
+	{
+		generator<int> my_iota = []() -> generator<int> { for (int i = 0;;) co_yield i++; }();
+
+		int i = 0;
+		for (int v : my_iota)
+		{
+			assert(i == v);
+			if (++i == 20) break;
+		}
+	}
 
 	std::cout << "all tests completed\n";
 
